739bc037f148add9410f0202ceecabe272574448
Brian Knapp <brianknapp@gmail.com>
2012-12-24 15:50:46 -0600

    Filled in web/app.rb file. For the moment things aren't going to be working, but it gives us a nice map of where we're going with the web app.

--- delivery/web/app.rb
+++ delivery/web/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'slim'
 
 require_relative '../../app/actions/list_statuses'
 
@@ -6,7 +7,81 @@ require_relative '../../external/status_jack'
 
 
 get '/' do
+  # get list of statuses 
   action = ListStatuses.new StatusJack.new
   @statuses = action.do
   @statuses.to_s
+
+  # get list of user
+  @users = ListUsers.new UserJack.new
+  @users.to_s
+  slim :index
+end
+
+get '/:user/create-status' do
+  slim :create_status
+end
+
+post '/:user/create-status' do
+  input = { :user_id => params[:user], :text => params[:text] }
+  action = CreateStatus.new StatusJack.new
+  @status = action.do input
+  redirect '/'
+end
+
+get '/sign-up' do
+  slim :sign_up
+end
+
+post '/sign-up' do
+  input = { :handle => params[:handle] }
+  action = CreateUser.new UserJack.new
+  @user = action.do input
+  redirect "/user/#{@user[:id]}"
+end
+
+get '/user/:id' do
+  input = { :id => input[:id] }
+  action = GetUser.new UserJack.new
+  @user = action.do input
+  slim :get_user
+end
+
+get '/status/:id' do
+  input = { :id => input[:id] }
+  action = GetStatus.new StatusJack.new
+  @status = action.do input
+  slim :get_status
+end
+
+get '/status/:id/update' do
+  input = { :id => input[:id] }
+  action = GetStatus.new StatusJack.new
+  @status = action.do input
+  slim :update_status
+end
+
+post '/status/:id/update' do
+  input = { :id => params[:id], :text => params[:text], :user_id => params[:user_id] }
+  action = UpdateStatus.new StatusJack.new
+  @status = action.do input
+  redirect "/status/#{@status[:id]}" 
+end
+
+get '/status/:id/remove' do
+  input = { :id => input[:id] }
+  action = GetStatus.new StatusJack.new
+  @status = action.do input
+  slim :remove_status
+end
+
+post '/status/:id/remove' do
+  input = { :id => params[:id] }
+  action = RemoveStatus.new StatusJack.new
+  result = action.do input
+  if result == true
+    redirect '/'
+  else
+    'ERROR'
+  end 
 end
