a4c0a6949d5c4341669369d357d98e82e11d289a
gabriel-dehan <dehan.gabriel@gmail.com>
2013-01-09 01:41:53 +0100

    Updates to go along with the contracts API change from https://github.com/gabriel-dehan/obvious/blob/feature/nicer-api-for-contracts/lib/obvious/contract.rb

--- app/contracts/status_jack_contract.rb
+++ app/contracts/status_jack_contract.rb
@@ -1,33 +1,30 @@
 require 'obvious'
 
 require_relative '../entities/status'
-   
+
 class StatusJackContract < Contract
-  def self.contracts
-    ['save', 'get', 'list', 'remove']
-  end
+  contracts :save, :get, :list, :remove
 
   def save_contract input
     input_shape = Status.shape
     output_shape = Status.shape
     call_method :save_alias, input, input_shape, output_shape
   end
-    
+
   def get_contract input
     input_shape = { :id => Fixnum }
-    output_shape = Status.shape 
+    output_shape = Status.shape
     call_method :get_alias, input, input_shape, output_shape
   end
-    
-  def list_contract 
-    output_shape = [ Status.shape ] 
+
+  def list_contract
+    output_shape = [ Status.shape ]
     call_method :list_alias, nil, nil, output_shape
   end
-    
+
   def remove_contract input
     input_shape = { :id => Fixnum }
-    output_shape = true 
+    output_shape = true
     call_method :remove_alias, input, input_shape, output_shape
   end
-     
 end
