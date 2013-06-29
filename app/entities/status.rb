0ddc7365d6dd8f97d1b84486552b427baca6635b
Brian Knapp <brianknapp@gmail.com>
2013-06-29 14:29:48 -0500

    [gh-11] - Updated Status to use Obvious::Entity

--- app/entities/status.rb
+++ app/entities/status.rb
@@ -1,33 +1,10 @@
-require 'validation'
+require 'obvious'
 
-class Status
-  include Validation
+class Status < Obvious::Entity
 
-  attr_validator :text, String
-  attr_validator :user_id, Fixnum 
-  attr_validator :id, Fixnum
-
-  # self.shape is useful for contract validation, especially on list and save methods
-  def self.shape
-    {
-      :text => String,
-      :user_id => Fixnum,
-      :id => Fixnum
-    }
-  end
-
-  def populate input
-    self.text = input[:text]
-    self.user_id = input[:user_id]
-    self.id = input[:id] 
-  end
-    
-  def to_hash
-    {
-      :text => text,
-      :user_id => user_id,
-      :id => id
-    } 
-  end
-    
+  value :text, String
+  value :user_id, Fixnum 
+  value :id, Fixnum
+   
 end
+
