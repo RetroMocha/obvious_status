6f385a391d65326205b39edeaf0f3be35e17f362
Brian Knapp <brianknapp@gmail.com>
2022-01-29 13:45:59 -0600

    fix: Removing rails app

--- delivery/rails_app/app/helpers/status_helper.rb
+++
@@ -1,2 +0,0 @@
-module StatusHelper
-end
--- delivery/rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_token = '36dc2b34193a9c1b232681d862484ea8379e948dd991ce5191af1a9c5fa77adaeea63aa67cf0f99d9be95ca629c41451b62892d893e25635583a05c5e3f1e719'
