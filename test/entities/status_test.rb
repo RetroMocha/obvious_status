36c8e2b09314f2255f20d5f813880f7575a53334
Brian Knapp <brianknapp@gmail.com>
2022-02-03 08:54:12 -0600

    refactor: Rebuild test for Status entity

---
+++ test/entities/status_test.rb
@@ -0,0 +1,17 @@
+require_relative '../test_helper'
+require_relative '../../app/entities/status'
+
+class StatusTest < Minitest::Test
+  def test_valid_input
+    status = Status.new(id: 1, user_id: 1, text: 'making a sandwich')
+    assert_equal(1, status.id)
+    assert_equal(1, status.user_id)
+    assert_equal('making a sandwich', status.text)
+  end
+
+  def test_invalid_input
+    assert_raises Obvious::TypeError do
+      Status.new(id: nil, user_id: nil, text: nil)
+    end
+  end
+end
