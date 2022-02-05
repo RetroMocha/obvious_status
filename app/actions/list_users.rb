2e09384ad1decc3c20d083247a502de65af907c1
Brian Knapp <brianknapp@gmail.com>
2022-02-05 09:14:27 -0600

    refactor: Rebuild test for ListUsers action

--- app/actions/list_users.rb
+++ app/actions/list_users.rb
@@ -1,12 +1,11 @@
 require_relative '../entities/user'
 
 class ListUsers
-  
   def initialize user_jack
     @user_jack = user_jack
   end
 
-  def execute
+  def exec
     user_data = @user_jack.list
     validate user_data
     user_data
@@ -14,7 +13,7 @@ class ListUsers
 
   def validate user_data
     # create/populate User objects for validation
-    user_data.each do |entry| 
+    user_data.each do |entry|
       user = User.new entry
     end
   end
